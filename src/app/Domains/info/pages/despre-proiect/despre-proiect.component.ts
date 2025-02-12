53a28b3eeedab84f2deea6283a5d6e2424b5956e
Alex Jacho <alexjacho77@gmail.com>
2025-02-12 18:06:44 +0200

    The page has been updated with basic information

--- src/app/Domains/info/pages/despre-proiect/despre-proiect.component.ts
+++ src/app/Domains/info/pages/despre-proiect/despre-proiect.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
 import { FooterComponent } from "../../../shared/components/footer/footer.component";
 import { HeaderComponent } from "../../../shared/components/header/header.component";
+import { RouterLinkWithHref } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-despre-proiect',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, HeaderComponent],
+  imports: [NavbarComponent, FooterComponent, HeaderComponent, RouterLinkWithHref, CommonModule,],
   templateUrl: './despre-proiect.component.html'
 })
 export class DespreProiectComponent {
